034c2a8ea0a0d847ef3ebc75a70aef5c45f24ed4
Aleksandra Koen <alvkoen@gmail.com>
2025-12-14 21:13:54 +0200

    Improve block validation

--- internal/planner/validator.go
+++ internal/planner/validator.go
@@ -3,9 +3,16 @@ package planner
 import (
 	"fmt"
 	"slices"
+	"time"
 )
 
 func ValidateBlocks(blocks []TimeBlock, busyBlocks []TimeBlock) error {
+	// Create map for O(1) busy block lookup instead of O(n) search
+	busySet := make(map[string]bool, len(busyBlocks))
+	for _, b := range busyBlocks {
+		busySet[blockKey(b)] = true
+	}
+
 	allBlocks := make([]TimeBlock, 0, len(blocks)+len(busyBlocks))
 	allBlocks = append(allBlocks, blocks...)
 	allBlocks = append(allBlocks, busyBlocks...)
@@ -16,37 +23,36 @@ func ValidateBlocks(blocks []TimeBlock, busyBlocks []TimeBlock) error {
 
 	for i := 0; i < len(allBlocks)-1; i++ {
 		if blocksOverlap(allBlocks[i], allBlocks[i+1]) {
-			block1 := allBlocks[i]
-			block2 := allBlocks[i+1]
+			block1, block2 := allBlocks[i], allBlocks[i+1]
 
-			isBusy1 := isInList(block1, busyBlocks)
-			isBusy2 := isInList(block2, busyBlocks)
+			isBusy1 := busySet[blockKey(block1)]
+			isBusy2 := busySet[blockKey(block2)]
 
 			if isBusy1 || isBusy2 {
-				busyBlock := block1
-				plannedBlock := block2
-				if !isBusy2 {
-					busyBlock = block2
-					plannedBlock = block1
+				plannedBlock, busyBlock := block1, block2
+				if isBusy1 {
+					plannedBlock, busyBlock = block2, block1
 				}
 				return fmt.Errorf("block '%s' (%s-%s) overlaps with busy time '%s' (%s-%s)",
-					plannedBlock.Title, plannedBlock.Start.Format(TimeFormat), plannedBlock.End.Format(TimeFormat),
-					busyBlock.Title, busyBlock.Start.Format(TimeFormat), busyBlock.End.Format(TimeFormat))
+					plannedBlock.Title, formatTime(plannedBlock.Start), formatTime(plannedBlock.End),
+					busyBlock.Title, formatTime(busyBlock.Start), formatTime(busyBlock.End))
 			}
 
 			return fmt.Errorf("blocks overlap: '%s' (%s-%s) and '%s' (%s-%s)",
-				block1.Title, block1.Start.Format(TimeFormat), block1.End.Format(TimeFormat),
-				block2.Title, block2.Start.Format(TimeFormat), block2.End.Format(TimeFormat))
+				block1.Title, formatTime(block1.Start), formatTime(block1.End),
+				block2.Title, formatTime(block2.Start), formatTime(block2.End))
 		}
 	}
 
 	return nil
 }
 
-func isInList(block TimeBlock, list []TimeBlock) bool {
-	return slices.ContainsFunc(list, func(b TimeBlock) bool {
-		return b.Title == block.Title && b.Start.Equal(block.Start) && b.End.Equal(block.End)
-	})
+func blockKey(b TimeBlock) string {
+	return b.Title + "|" + b.Start.String() + "|" + b.End.String()
+}
+
+func formatTime(t time.Time) string {
+	return t.Format(TimeFormat)
 }
 
 func blocksOverlap(a, b TimeBlock) bool {
